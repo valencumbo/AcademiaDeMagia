d9135241168d002f11a756c7ac8f40839f10f9de
JuanIAlegre <juanignacioalegre1@gmail.com>
2024-10-27 14:46:41 -0300

    Primer Hechizo + test
    Instancia Eq para Mago y Hechizo requeridos para los test
    
    Implementacion  lagrimaFenix

--- academia-magica/src/Academia.hs
+++ academia-magica/src/Academia.hs
@@ -1,18 +1,31 @@
 module Academia where
-
+    
 import PdePreludat
-import Data.Int
 
 data Mago = Mago{
     nombre :: String,
-    edad :: Int,
-    salud :: Int,
+    edad :: Number,
+    salud :: Number,
     hechizos :: [Hechizo]
 } deriving (Show)
+
+instance Eq Mago where
+    (Mago nombre edad salud hechizos) == (Mago otroNombre otraEdad otraSalud otroHechizos) =
+        nombre == otroNombre && edad == otraEdad && salud == otraSalud && hechizos == otroHechizos
 
 data Hechizo = Hechizo{
     nombreHechizo :: String,
     efecto :: EfectoHechizo
 } deriving (Show)
 
+instance Eq Hechizo where
+    (Hechizo nombreHechizo _) == (Hechizo otroNombreHechizo _) =
+        nombreHechizo == otroNombreHechizo
+
 type EfectoHechizo = Mago -> Mago
+
+lagrimaFenix :: Number -> EfectoHechizo
+lagrimaFenix curacion mago = mago{salud = salud mago + curacion}
+
+potter = Mago{nombre = "Harry", edad = 20, salud = 100, hechizos = [lagrima]}
+lagrima = Hechizo{nombreHechizo = "lagrimaFenix", efecto = lagrimaFenix 30}
--- academia-magica/src/Spec.hs
+++ academia-magica/src/Spec.hs
@@ -1,6 +1,8 @@
 module Spec where
+  
 import PdePreludat
 import Library
+import Academia
 import Test.Hspec
 
 correrTests :: IO ()
@@ -8,3 +10,12 @@ correrTests = hspec $ do
   describe "Test de ejemplo" $ do
     it "El pdepreludat se instaló correctamente" $ do
       doble 1 `shouldBe` 2
+    it "Mago afectado por curacion" $ do
+      lagrimaFenix 50 potter `shouldBe` Mago{nombre = "Harry", edad = 20, salud = 150, hechizos = [lagrima]}
+      lagrimaFenix 0 potter `shouldBe` potter
+
+
+
+
+
+
