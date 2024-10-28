5c383582d973d837594663e7a170574771cb8385
JuanIAlegre <juanignacioalegre1@gmail.com>
2024-10-27 21:09:21 -0300

    NuevosHechizos + Test
    - Implementacio SectumSempra - obliviate  mas test
    - Problemas con valores de salud no pares, redondeo para arriba

--- academia-magica/src/Academia.hs
+++ academia-magica/src/Academia.hs
@@ -27,5 +27,22 @@ type EfectoHechizo = Mago -> Mago
 lagrimaFenix :: Number -> EfectoHechizo
 lagrimaFenix curacion mago = mago{salud = salud mago + curacion}
 
+sectumSempra :: EfectoHechizo
+sectumSempra mago = mago{salud = salud mago - efectoSectum (salud mago)}
+
+efectoSectum :: Number -> Number
+efectoSectum salud | salud > 10 = 10
+                   | otherwise  = div salud 2
+
+obliviate :: Number -> EfectoHechizo
+obliviate olvidados mago = mago{hechizos = drop olvidados (hechizos mago)}
+
 potter = Mago{nombre = "Harry", edad = 20, salud = 100, hechizos = [lagrima]}
+weasley = Mago{nombre = "Ron", edad = 21, salud = 5, hechizos = [lagrima, sectum]}
+malfoy = Mago{nombre = "Draco", edad = 22, salud = 10, hechizos = [lagrima, sectum]}
+goyle = Mago{nombre = "Gregory", edad = 20, salud = 2, hechizos = []}
+granger = Mago{nombre = "Hermione", edad = 21, salud = 70, hechizos = [lagrima, sectum, obliviateHechizo]}
+
 lagrima = Hechizo{nombreHechizo = "lagrimaFenix", efecto = lagrimaFenix 30}
+sectum = Hechizo{nombreHechizo = "sectumSempra", efecto = sectumSempra}
+obliviateHechizo = Hechizo{nombreHechizo = "obliviate", efecto = obliviate 2}
--- academia-magica/src/Spec.hs
+++ academia-magica/src/Spec.hs
@@ -12,10 +12,15 @@ correrTests = hspec $ do
       doble 1 `shouldBe` 2
     it "Mago afectado por curacion" $ do
       lagrimaFenix 50 potter `shouldBe` Mago{nombre = "Harry", edad = 20, salud = 150, hechizos = [lagrima]}
-      lagrimaFenix 0 potter `shouldBe` potter
-
-
-
+      lagrimaFenix 0 potter `shouldBe` Mago{nombre = "Harry", edad = 20, salud = 100, hechizos = [lagrima]}
+    it "Mago afectado por sectum" $ do
+      sectumSempra potter `shouldBe` Mago{nombre = "Harry", edad = 20, salud = 90, hechizos = [lagrima]}
+      -- sectumSempra weasley `shouldBe` Mago{nombre = "Ron", edad = 21 , salud = 2.5, hechizos = [lagrima, sectum]}
+      sectumSempra malfoy `shouldBe` Mago{nombre = "Draco", edad = 22 , salud = 5, hechizos = [lagrima, sectum]}
+    it "Mago afectado por obliviate" $ do
+      obliviate 1 potter `shouldBe` Mago{nombre = "Harry", edad = 20, salud = 100, hechizos = []}
+      obliviate 2 granger `shouldBe` Mago{nombre = "Hermione", edad = 21, salud = 70, hechizos = [obliviateHechizo]}
+      obliviate 10 goyle `shouldBe` Mago{nombre = "Gregory", edad = 20, salud = 2, hechizos = []}
 
 
 
